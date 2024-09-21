9921b35209f834ce8f3d44a8431153735863fd69
rami510 <rami.bouabid@gmail.com>
2024-09-21 14:04:49 +0200

    Initial commit of rover project

---
+++ src/main/java/MarsRover.java
@@ -0,0 +1,11 @@
+public class MarsRover {
+
+    private int x, y;
+    private char direction;
+
+    public MarsRover(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+}
